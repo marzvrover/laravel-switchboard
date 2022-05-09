29f1eacaaa9705aca4c39a5faa2f486d29c83ffd
Marz Rover <marz@marzvrover.com>
2022-05-09 01:15:23 -0400

    Throw error if Illuminate is not installed

--- src/LaravelSwithboardServiceProvider.php
+++ src/LaravelSwithboardServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace Marzvrover\LaravelSwitchboard;
 
+if (! \Composer\InstalledVersions::isInstalled('Illuminate\Support') ) {
+    throw new \Exception("Must install Illuminate\Support for laravel support");
+}
+
 use Illuminate\Support\ServiceProvider;
 
 class LaravelSwithboardServiceProvider extends ServiceProvider {
